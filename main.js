8d215f55ca60cd2d30b9d0d4eb843ddfc9930be5
akulOn <dracaluka12@gmail.com>
2025-05-20 18:45:39 +0200

    added contact dialog

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello!");
+const dialog = document.getElementById("dialog");
+const footer = document.getElementById("footer");
+
+footer.addEventListener("click", (event) => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("click", (event) => {
+  const rect = dialog.getBoundingClientRect();
+  const clickedOutside =
+    event.clientX < rect.left ||
+    event.clientX > rect.right ||
+    event.clientY < rect.top ||
+    event.clientY > rect.bottom;
+
+  if (clickedOutside) {
+    dialog.close();
+  }
+});
+
+dialog.addEventListener("blur", () => {
+  dialog.close();
+});
